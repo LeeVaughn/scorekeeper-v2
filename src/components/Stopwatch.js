a3e1c32062945c98732c84af59002d7813177cec
LeeVaughn <dlvprogramming@gmail.com>
2019-04-06 20:56:27 -0700

    created state and function to track if timer is running and update button

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
 
 class Stopwatch extends Component {
+  state = {
+    isRunning: false
+  }
+
+  handleStopwatch = () => {
+    // toggles isRunning to the opposite of its current value
+    this.setState({
+      isRunning: !this.state.isRunning
+    })
+  }
+
   render() {
     return (
       <div className="stopwatch">
         <h2>Stopwatch</h2>
         <span className="stopwatch-time">0</span>
-        <button>Start</button>
+        <button onClick={ this.handleStopwatch }>
+          {/* button will display Stop if true, Start if false */}
+          { this.state.isRunning ? "Stop" : "Start" }
+        </button>
         <button>Reset</button>
       </div>
     );
