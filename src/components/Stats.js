249441ec9ed6fb71600bbf6b07da09c5a9199f75
LeeVaughn <dlvprogramming@gmail.com>
2019-04-18 21:07:00 -0700

    imported Consumer from Context.js and used it instead of props

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,37 +1,37 @@
 import React from "react";
-import PropTypes from "prop-types";
+import { Consumer } from "./Context";
 
 const Stats = (props) => {
-  const totalPlayers = props.players.length;
-  /**
-   * iterates over each player in state to get the total number of points
-   * @param (number) total - accumulator to add up the returned values
-   * @param (object) player - current item from the array being processed
-   */
-  const totalPoints = props.players.reduce( (total, player) => {
-    return total + player.score;
-  }, 0);
-
   return(
-    <table className="stats">
-      <tbody>
-        <tr>
-          <td>Players:</td>
-          <td>{ totalPlayers }</td>
-        </tr>
-        <tr>
-          <td>Total Points:</td>
-          <td>{ totalPoints }</td>
-        </tr>
-      </tbody>
-    </table>
+    <Consumer>
+      { context => {
+        const totalPlayers = context.length;
+        /**
+        * iterates over each player in state to get the total number of points
+        * @param (number) total - accumulator to add up the returned values
+        * @param (object) player - current item from the array being processed
+        */
+        const totalPoints = context.reduce( (total, player) => {
+          return total + player.score;
+        }, 0);
+
+        return (
+          <table className="stats">
+            <tbody>
+              <tr>
+                <td>Players:</td>
+                <td>{ totalPlayers }</td>
+              </tr>
+              <tr>
+                <td>Total Points:</td>
+                <td>{ totalPoints }</td>
+              </tr>
+            </tbody>
+          </table>
+        );
+      }}
+    </Consumer>
   );
 }
-
-Stats.propTypes = {
-  players: PropTypes.arrayOf(PropTypes.shape({
-    score: PropTypes.number
-  }))
-};
 
 export default Stats;
