0bd4124caabdde4fd6120e8f591a4d538df620d4
LeeVaughn <dlvprogramming@gmail.com>
2019-04-20 20:16:39 -0700

    moved state and function to index.js and imported and added Provider

--- src/components/App.js
+++ src/components/App.js
@@ -1,112 +1,44 @@
 import React, { Component } from "react";
 
 // import components
-import { Provider } from "./Context";
+import { Consumer } from "./Context";
 import Header from "./Header";
 import Player from "./Player";
 import AddPlayerForm from "./AddPlayerForm"
 
 class App extends Component {
-  // initializes a player state using a class property
-  state = {
-    players: [
-      {
-        name: "Guil",
-        score: 0,
-        id: 1
-      },
-      {
-        name: "Treasure",
-        score: 0,
-        id: 2
-      },
-      {
-        name: "Ashley",
-        score: 0,
-        id: 3
-      },
-      {
-        name: "James",
-        score: 0,
-        id: 4
-      }
-    ]
-  };
-
-  // player id counter
-  prevPlayerId = 4;
-
-  getHighScore = () => {
-    const scores = this.state.players.map( p => p.score );
-    // returns the largest score
-    const highScore = Math.max(...scores);
-    if (highScore) {
-      return highScore;
-    } 
-    return null;
-  }
-
-  handleScoreChange = (index, delta) => {
-    this.setState( prevState => ({
-      score: prevState.players[index].score += delta
-    }));
-  }
-
-  handleAddPlayer = (name) => {
-    this.setState( prevState => {
-      return {
-        players: [
-          // uses the spread operator to merge the initial objects in players state with the new array being created below
-          ...prevState.players,
-          {
-            name: name,
-            score: 0,
-            id: this.prevPlayerId += 1
-          }
-        ]
-      }
-    });
-  }
-
-  handleRemovePlayer = (id) => {
-    this.setState( prevState => {
-      return {
-        // produces a new array that no longer contains the player
-        // p represents the current item being processed, returns all player objects in state except for the matching id
-        players: prevState.players.filter( p => p.id !== id )        
-      };
-    });
-  }
-
   render() {
-    const highScore = this.getHighScore();
-
     return (
-      <Provider value={ this.state.players }>
-        <div className="scoreboard">
-          <Header />
-    
-          {/* players list */}
-          {/* iterates over array
-          map takes a callback function that receives and processes each item in the array and returns a new array
-          player represents the current item being processed */}
-          { this.state.players.map( (player, index) =>
-            <Player
-              name={ player.name }
-              score={ player.score }
-              id = { player.id }
-              key={ player.id.toString() }
-              index={ index }
-              changeScore={ this.handleScoreChange }
-              removePlayer={ this.handleRemovePlayer }
-              // checks to see if player's score is equal to high score
-              isHighScore={ highScore === player.score}
-            />
-          )}
+      <Consumer>
+        { context => {
+          const highScore = context.actions.getHighScore();
+          return (
+            <div className="scoreboard">
+              <Header title="Scoreboard" />
+        
+              {/* players list */}
+              {/* iterates over array
+              map takes a callback function that receives and processes each item in the array and returns a new array
+              player represents the current item being processed */}
+              { context.players.map( (player, index) =>
+                <Player
+                  name={ player.name }
+                  score={ player.score }
+                  id = { player.id }
+                  key={ player.id.toString() }
+                  index={ index }
+                  changeScore={ context.actions.handleScoreChange }
+                  removePlayer={ context.actions.handleRemovePlayer }
+                  // checks to see if player's score is equal to high score
+                  isHighScore={ highScore === player.score}
+                />
+              )}
 
-          <AddPlayerForm addPlayer={ this.handleAddPlayer } />
-        </div>
-      </Provider>
+              <AddPlayerForm addPlayer={ context.actions.handleAddPlayer } />
+            </div>
+          );
+        }}
+      </Consumer>
     );    
   }
 }
