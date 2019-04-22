2729a7cb97cffb42d4792f123423741344292b0f
LeeVaughn <dlvprogramming@gmail.com>
2019-04-22 10:20:24 -0700

    streamlined props and how data is passed

--- src/components/Player.js
+++ src/components/Player.js
@@ -9,16 +9,13 @@ import Icon from "./Icon";
 class Player extends PureComponent {
   // defines propTypes inside of the class component
   static propTypes = {
-    name: PropTypes.string.isRequired,
-    score: PropTypes.number.isRequired,
-    id: PropTypes.number,
     index: PropTypes.number,
     isHighScore: PropTypes.bool
   };
 
   render() {
     // uses variable assignment for destructuring of props
-    const { id, isHighScore, name, score, index } = this.props;
+    const { isHighScore, index } = this.props;
 
     return (
       <div className="player">
@@ -26,19 +23,16 @@ class Player extends PureComponent {
           { context => {
             return (
               <span className="player-name">
-                <button className="remove-player" onClick={ () => context.actions.removePlayer(id) }>✖</button>
+                <button className="remove-player" onClick={ () => context.actions.removePlayer(context.players[index].id) }>✖</button>
 
                 <Icon isHighScore={ isHighScore } />
-                { name }
+                { context.players[index].name }
               </span>
             );
           }}
         </Consumer>
         
-        <Counter
-          score={ score }
-          index={ index }
-        />
+        <Counter index={ index } />
       </div>
     );
   }
